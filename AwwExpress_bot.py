fd47aa0c44cd763178e3971764d51a688f1ffa5e
Jerry Ho <a29988122@gmail.com>
2018-08-23 19:10:08 +0800

    Now it's a Python 3 script; also added simple error handling.

--- AwwExpress_bot.py
+++ AwwExpress_bot.py
@@ -1,32 +1,28 @@
-# -*- coding: utf-8 -*-
-from __future__ import print_function
 import subprocess
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import Popen, PIPE, STDOUT, check_output
 import requests
 import logging
 import json
-import ConfigParser
-import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
+import configparser
 import telegram
 import telegram.ext
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, MessageHandler, Filters
 import dropbox
 
-config = ConfigParser.ConfigParser()
-config.readfp(open(r'config.ini'))
-bot_token = config.get('Telegram', 'bot_token')
-dropbox_token = config.get('Dropbox', 'dropbox_token')
-upload_to_folder = config.get('Dropbox', 'upload_to_folder')
-id = config.get('Streamable', 'id')
-pw = config.get('Streamable', 'pw')
+config = configparser.ConfigParser()
+config.read_file(open("config.ini"))
+bot_token = config.get("Telegram", "bot_token")
+dropbox_token = config.get("Dropbox", "dropbox_token")
+upload_to_folder = config.get("Dropbox", "upload_to_folder")
+id = config.get("Streamable", "id")
+pw = config.get("Streamable", "pw")
 
+# TBD: formal logging.
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO)
 
-#telegram.ext https://python-telegram-bot.org
+# telegram.ext https://python-telegram-bot.org
 updater = Updater(token=bot_token)
 dispatcher = updater.dispatcher
 
@@ -38,7 +34,7 @@ class TransferData:
         """upload a file to Dropbox using API v2
         """
         dbx = dropbox.Dropbox(self.access_token)
-        with open(file_from, 'rb') as f:
+        with open(file_from, "rb") as f:
             dbx.files_upload(f.read(), file_to, mute=True)
     
     def share_link(self, file_to):
@@ -50,37 +46,37 @@ class TransferData:
 def vreddit(bot, update):
     #greet
     redditurl = update.message.text
-    bot.send_message(chat_id=update.message.chat_id, text='안녕하세요, 저는 AwwExpress_bot입니다. 잠시만 기다려주세요!')
+    bot.send_message(chat_id=update.message.chat_id, text="안녕하세요, 저는 AwwExpress_bot입니다. 잠시만 기다려주세요!")
 
     #youtube-dl
-    dlprocess = subprocess.Popen(['youtube-dl', redditurl, '--restrict-filenames'], stdout=PIPE, stderr=STDOUT, bufsize=1, universal_newlines=True)
-    dlprocess.wait()
-    bot.send_message(chat_id=update.message.chat_id, text='File downloaded successfully from reddit.\nI live beside reddit\'s server room, that\'s the secret.\nI\'m quite fast, right?')
+    dlprocess = subprocess.Popen(["youtube-dl", redditurl, "--restrict-filenames"], stderr=PIPE, bufsize=1, universal_newlines=True)
+    out, err = dlprocess.communicate()
+    if err:
+        print(err)
+        bot.send_message(chat_id=update.message.chat_id, text=err)
+        return
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text="File downloaded successfully from reddit.\nI live beside reddit\"s server room, that\"s the secret.\nI\"m quite fast, right?")
 
     #fetch filename and pass to streamable
-    nameprocess = subprocess.Popen(['youtube-dl', redditurl, '--no-warnings', '--restrict-filenames', '--get-filename'], stdout=PIPE, stderr=STDOUT, bufsize=1, universal_newlines=True)
-    youtubeoutputstring = ""
-    for line in nameprocess.stdout:
-        print(line, end='') 
-        youtubeoutputstring+=str(line)
-    nameprocess.wait()
-    youtubeoutputstring = str(youtubeoutputstring).decode('utf8')
+    youtubeoutputstring = check_output(["youtube-dl", redditurl, "--no-warnings", "--restrict-filenames", "--get-filename"], universal_newlines=True)
+    youtubeoutputstring = str(youtubeoutputstring)
     youtubeoutputstring = youtubeoutputstring.rstrip()
 
     #streamable
-    # url = 'https://api.streamable.com/upload'
+    # url = "https://api.streamable.com/upload"
     # headers = {
-    # 'User-Agent': 'My Personal Bot QQ',
-    # 'From': 'a29988122@gmail.com'  # This is another valid field
+    # "User-Agent": "My Personal Bot QQ",
+    # "From": "a29988122@gmail.com"  # This is another valid field
     # }
-    # files = {'file': open(youtubeoutputstring, 'rb')}
+    # files = {"file": open(youtubeoutputstring, "rb")}
     # r = requests.post(url, auth=(id, pw), files=files, headers=headers)
-    # bot.send_message(chat_id=update.message.chat_id, text='少々お待ちください、streamableサーバーにアップロードしています。')
+    # bot.send_message(chat_id=update.message.chat_id, text="少々お待ちください、streamableサーバーにアップロードしています。")
     
     # json_data = json.loads(r.text)
     # json_data = json_data["shortcode"]
-    # print('Generated link: https://streamable.com/'+json_data)
-    # bot.send_message(chat_id=update.message.chat_id, text='我幫你解決了討人厭又打不開的v.reddit.com！來，這裡是你的可愛圖： https://streamable.com/'+json_data)
+    # print("Generated link: https://streamable.com/"+json_data)
+    # bot.send_message(chat_id=update.message.chat_id, text="我幫你解決了討人厭又打不開的v.reddit.com！來，這裡是你的可愛圖： https://streamable.com/"+json_data)
 
     #dropbox
     access_token = dropbox_token
@@ -91,8 +87,8 @@ def vreddit(bot, update):
     # API v2
     transferData.upload_file(file_from, file_to)
     url = transferData.share_link(file_to)
-    print('Generated link: '+url)
-    bot.send_message(chat_id=update.message.chat_id, text='我幫你解決了討人厭又打不開的v.reddit.com！\n來，這裡是你的可愛圖：'+ url)
+    print("Generated link: "+url)
+    bot.send_message(chat_id=update.message.chat_id, text="我幫你解決了討人厭又打不開的v.reddit.com！\n來，這裡是你的可愛圖："+ url)
 
 
 #trigger event of vreddit when Filters.text message detected
